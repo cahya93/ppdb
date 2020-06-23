b33b323d04bd7d4374c587ae5979e833740236d1
Eko Cahyanto <49233072+cahya93@users.noreply.github.com>
2020-06-23 19:22:38 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://server-muhka.ddns.net/ppdb';
+$config['base_url'] = 'http://server-muhka.ddns.net:8080/ppdb';
 
 /*
 |--------------------------------------------------------------------------
